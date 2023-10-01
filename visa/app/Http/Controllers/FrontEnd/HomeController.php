35e7e0c87cf5a5e952669e17378598772b1195cc
Darshantada10 <darshantadaofficial@gmail.com>
2023-10-01 20:26:59 +0530

    update

--- visa/app/Http/Controllers/FrontEnd/HomeController.php
+++ visa/app/Http/Controllers/FrontEnd/HomeController.php
@@ -2,10 +2,42 @@
 
 namespace App\Http\Controllers\FrontEnd;
 
-use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use App\Models\Logo;
+use App\Models\About;
+use App\Models\Navbar;
+use App\Models\Slider;
+use App\Models\Benefit;
+use App\Models\VisaBox;
+use App\Models\SocialTags;
+use App\Models\VideoTitle;
+use App\Models\VisaProcess;
 use Illuminate\Http\Request;
+use App\Models\ContactInformation;
+use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('FrontEnd.Home.index');
+    }
+    public function GetActiveData()
+    {
+        $logo = Logo::where('visible', true)->orderBy('created_at', 'desc')->first(['logo']);
+        $navbar = Navbar::all();
+        $slider = Slider::where('visible', true)->orderBy('created_at', 'desc')->first(['heading','sub_heading']);
+        $social_tags = SocialTags::where('visible', true)->orderBy('created_at', 'desc')->first(['instagram','facebook','twitter','youtube']);
+        $contact_info = ContactInformation::where('visible', true)->orderBy('created_at', 'desc')->first(['mobile1','mobile2','address']);
+        $visa_boxes = VisaBox::where('visible', '=', '1')->orderBy('created_at', 'desc')->take(5)->get(['title', 'small_description', 'image']);
+        $about = About::where('visible', true)->orderBy('created_at', 'desc')->first(['heading','description',
+        'point_1','point_2','description_1','description_2']);
+        $video_title = VideoTitle::where('visible', true)->orderBy('created_at', 'desc')->first(['title','url']);
+        $benefit = Benefit::where('visible', true)->orderBy('created_at', 'desc')->first(['heading1','heading2','heading3','heading4','point1','point2','point3','point4']);
+        $visa_process = VisaProcess::where('visible', true)->orderBy('created_at', 'desc')->first(['heading1','heading2','heading3','point1','point2','point3']);
+        $blogs = Blog::where('visible', '=', '1')->orderBy('created_at', 'desc')->take(3)->get();
+
+        return response()->json([$logo,$navbar,$slider,$social_tags,$contact_info,$visa_boxes,$about,$video_title,$benefit,$visa_process,$blogs]);
+
+    }
 }
--- visa/routes/api.php
+++ visa/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrontEnd\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,38 +19,38 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Logo Routes
-Route::get('/logo',[HomeController::class,'getActiveLogo']);
+//Home Routes
+Route::get('/ahpd',[HomeController::class,'GetActiveData']);
 
-//Navbar Routes
-Route::get('/navbar',[HomeController::class,'getActiveNavbar']);
+// //Navbar Routes
+// Route::get('/navbar',[HomeController::class,'getActiveNavbar']);
 
-//Slider Routes
-Route::get('/slider',[HomeController::class,'getActiveSlider']);
+// //Slider Routes
+// Route::get('/slider',[HomeController::class,'getActiveSlider']);
 
-//Visa Box Routes
-Route::get('/visa-box',[HomeController::class,'getActiveVisaBox']);
+// //Visa Box Routes
+// Route::get('/visa-box',[HomeController::class,'getActiveVisaBox']);
 
-//Visa Box Routes
-Route::get('/about',[HomeController::class,'getActiveAbout']);
+// //Visa Box Routes
+// Route::get('/about',[HomeController::class,'getActiveAbout']);
 
-//Contact Info Routes
-Route::get('/contact-info',[HomeController::class,'getActiveContactInfo']);
+// //Contact Info Routes
+// Route::get('/contact-info',[HomeController::class,'getActiveContactInfo']);
 
-//Social Tags Routes
-Route::get('/social-tags',[HomeController::class,'getActiveSocialTags']);
+// //Social Tags Routes
+// Route::get('/social-tags',[HomeController::class,'getActiveSocialTags']);
 
-//Video title Routes
-Route::get('/video-title',[HomeController::class,'getActiveVideoTitle']);
+// //Video title Routes
+// Route::get('/video-title',[HomeController::class,'getActiveVideoTitle']);
 
-//Benefits Routes
-Route::get('/benefits',[HomeController::class,'getActiveBenefits']);
+// //Benefits Routes
+// Route::get('/benefits',[HomeController::class,'getActiveBenefits']);
 
-//Process Routes
-Route::get('/visa-process',[HomeController::class,'getActiveVisaProcess']);
+// //Process Routes
+// Route::get('/visa-process',[HomeController::class,'getActiveVisaProcess']);
 
-//blogs Routes
-Route::get('/blogs',[HomeController::class,'getActiveBlogs']);
+// //blogs Routes
+// Route::get('/blogs',[HomeController::class,'getActiveBlogs']);
 
 //Single Blog Routes
 Route::get('/blogs-page',[BlogController::class,'getActiveBlogs']);
